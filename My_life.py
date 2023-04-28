74238b6b602e5e8833941a5eb154ef486a3fafd5
Elyza <elyzajanerelucio@gmail.com>
2023-04-28 09:03:42 +0800

    Write the line to the file

--- My_life.py
+++ My_life.py
@@ -6,8 +6,9 @@ def write_to_file():
             while True:
                 # Ask the user to input a line of text
                 line = input("Enter line: ")
-                
-# Write the line to the file
+
+                # Write the line to the file
+                line = input("Enter line: ")
 # Ask the user if there are more lines to enter
 # Print a message indicating the file has been written successfully
 # If an error occurs while writing to the file, ptint an error message
