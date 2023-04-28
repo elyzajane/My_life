6e572d9e0291b774e6af96379af69fdea8d3e52a
Elyza <elyzajanerelucio@gmail.com>
2023-04-28 09:02:03 +0800

    Ask the user to input text

--- My_life.py
+++ My_life.py
@@ -2,8 +2,11 @@ def write_to_file():
     try:
        # Open a writing file
        with open("mylife.txt", "w") as file: 
-# Ask the user to input and write to file
-# Ask the user to input a line of text
+            # Ask the user to input and write to file
+            while True:
+                # Ask the user to input a line of text
+                line = input("Enter line: ")
+                
 # Write the line to the file
 # Ask the user if there are more lines to enter
 # Print a message indicating the file has been written successfully
